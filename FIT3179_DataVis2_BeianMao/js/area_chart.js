79825424f6b2f8137ee2cf3154435c04d58db8d4
BeianAnnieMao31494153 <145409689+BeianAnnieMao31494153@users.noreply.github.com>
2023-10-09 23:19:31 +1100

    Reupload of Draft 1 in the same folder

---
+++ FIT3179_DataVis2_BeianMao/js/area_chart.js
@@ -0,0 +1,4 @@
+var vg_2 = "./js/area_chart.json";
+vegaEmbed("#area_chart", vg_2).then(function (result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
---
+++ FIT3179_DataVis2_BeianMao/js/bar_chart.js
@@ -0,0 +1,4 @@
+var vg_3 = "./js/bar_chart.json";
+vegaEmbed("#bar_chart", vg_3).then(function (result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
